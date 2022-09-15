213a42d56a1a469d8b74e2a7790c51c8321ad74a
mengchain <113321056+mengchain@users.noreply.github.com>
2022-09-15 13:41:57 +0800

    Initial Checkin

---
+++ scripts/recover/mount.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -e
+
+print () {
+    echo -e "\n\033[1m> $1\033[0m\n"
+}
+
+# Set DISK
+select ENTRY in $(ls /dev/disk/by-id/);
+do
+    DISK="/dev/disk/by-id/$ENTRY"
+    echo "Mounting $ENTRY."
+    break
+done
+
+print "Load ZFS module"
+modprobe zfs
+
+print "Reimport zpool"
+if zpool status zroot &>/dev/null
+then
+  zpool export zroot
+fi
+zpool import -d /dev/disk/by-id -R /mnt zroot -N -f
+
+print "Load ZFS keys"
+zfs load-key -L prompt zroot
+
+print "Mount ROOT dataset"
+select ENTRY in $(zfs list | awk '/ROOT\// {print $1}')
+do
+    echo "Mount $ENTRY as slash dataset."
+    zfs mount "$ENTRY"
+    break
+done
+
+print "Mount datasets"
+zfs mount -a
+
+print "Mount EFI part"
+EFI="$DISK-part1"
+mount "$EFI" /mnt/efi
+
+# Init chroot
+mount --rbind /sys /mnt/sys
+mount --make-rslave /mnt/sys
+mount --rbind /dev /mnt/dev
+mount --make-rslave /mnt/dev
+mount --rbind /proc /mnt/proc
+mount --make-rslave /mnt/proc
+
+# Finish
+echo -e '\e[32mAll OK\033[0m'
